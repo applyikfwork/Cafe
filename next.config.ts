b874465c5f8be5a666e458fa58e989e074b61b45
Discountedmarket <discountedmarkets@gmail.com>
2025-11-26 09:06:33 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  devIndicators: {
+    buildActivity: false,
+  },
+  experimental: {
+    allowedDevOrigins: [
+      'https://6000-firebase-studio-1764069556200.cluster-6dx7corvpngoivimwvvljgokdw.cloudworkstations.dev',
+    ],
+  },
 };
 
 export default nextConfig;
